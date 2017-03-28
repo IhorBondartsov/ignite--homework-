ef877e5192e8fb29977308f184f665e2256b8ff1
Ihor Bondartsov <bondartsov.igor@gmail.com>
2017-03-29 00:32:47 +0300

    add new task

--- lesson_005/Classwork/classwork_lesson_005.js
+++ lesson_005/Classwork/classwork_lesson_005.js
@@ -11,7 +11,27 @@ function printNumber() {
 };
 printNumber();
 //Task 2
-
-
-
+var leader = {
+    name: "Василий Иванович",
+    age: 35
+};
+userObj = JSON.stringify(leader);
+alert(userObj);
+userObj2 = JSON.parse(userObj);
+alert(userObj2);
 //Task 3
+function argum(something) {
+    try {
+        if (arguments.length > 1) {
+            throw new Error("You must use only one argument");
+        }
+        return something;
+    }
+    catch (e) {
+        alert(e); // referenceError;
+    }
+
+};
+
+console.log(argum(3,43));
+console.log(argum(343));
